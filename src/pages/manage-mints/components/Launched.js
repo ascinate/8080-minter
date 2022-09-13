425c556e026802c8693990b0fbedcf916262b47a
News <samrat.subratadas@gmail.com>
2022-09-13 20:06:50 +0530

    new launched page

--- src/pages/manage-mints/components/Launched.js
+++ src/pages/manage-mints/components/Launched.js
@@ -11,7 +11,7 @@ function Launched(){
         <>
         {
             lonchedData.map((curElem) =>{
-                const { id, titel, link, editlink, colletion, balance, auhor, pertage} = curElem;
+                const { id, titel, link, owner , url , minted , editlink, colletion, balance} = curElem;
                 return(
                     <>
                    
@@ -21,51 +21,113 @@ function Launched(){
                             <div className="itesm-launged">
                                 <div className="row">
                                     <div className="col-lg-8">
-                                        <h2 className="long-titel"> {titel} </h2>
-                                        <p className="author-dl"> <NavLink to={link}>
-                                        jirafammint <FaExternalLinkAlt/>
-                                        </NavLink> •{pertage} Minted • {auhor} </p>
-                                    </div>
-                                    <div className="col-lg-4">
-                                        <p className="lon-time"> Launched on 2nd Jan, 2022 </p>
-                                        <ul className="filed-edit d-flex align-items-center">
+                                        <h2 className="long-titel text-white"> {titel} </h2>
+
+                                        <ul className="filed-edit d-flex align-items-center justify-content-start list-unstyled">
                                             <li>
                                             <NavLink to={editlink}> EDIT </NavLink>
                                             </li>
                                             <li>
                                             <NavLink to={link}> PAUSE </NavLink>
                                             </li>
+                                            
+                                            <li>
+                                               <NavLink to={link}> Manage Allowlist </NavLink>
+                                            
+                                            </li>
+                                            <li>
+                                               <NavLink to={link}> Reveal </NavLink>
+                                            
+                                            </li>
                                             <li>
                                             <NavLink to={link}> PREVIEW </NavLink>
                                             </li>
                                         </ul>
+
+                                     
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <p className="lon-time"> Launched on 2nd Jan, 2022 </p>
+                                        
                                     </div>
                                 </div>
 
                                 <div className="loung-deta">
-                                    <div className="row row-cols-1 row-cols-lg-2">
-                                    <div className="col">
-                                        <div className="comon-lounched1 text-center">
-                                            <h3> Total Collection </h3>
-                                            <h2> {colletion} </h2> 
+                                    <div className="row row-cols-1 row-cols-lg-5 g-lg-3">
+                                        
+                                        <div className="col">
+                                            <div className="comon-lounched1 text-center">
+                                            <div className="comon-top-l">
+                                                    <h5> URL Slug </h5>
+                                            </div>
+                                            <div className="comon-top-b">
+                                                    <NavLink to={link}> 
+                                                        jirafammint <FaExternalLinkAlt/>
+                                                    </NavLink>
+                                            </div>
+                                                
+                                                
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col">
-                                        <div className="comon-lounched1 text-center">
-                                            <h3> Contract Balance </h3>
-                                            <h2> {balance} </h2> 
-                                            <h3> WITHDRAW </h3>
+
+                                        <div className="col">
+                                           <div className="comon-lounched1 text-center">
+                                                <div className="comon-top-l">
+                                                        <h5> Minted </h5>
+                                                </div>
+                                                <div className="comon-top-b">
+                                                        <p> {minted}</p>
+                                                </div>
+                                                
+                                                
+                                            </div>
                                         </div>
-                                    </div>
+
+
+                                        <div className="col">
+                                            <div className="comon-lounched1 text-center">
+                                                <div className="comon-top-l">
+                                                        <h5> Owners </h5>
+                                                </div>
+                                                <div className="comon-top-b">
+                                                     <p> {owner}</p>
+                                                </div>
+                                                
+                                                
+                                            </div>
+                                        </div>
+
+                                        <div className="col">
+                                            <div className="comon-lounched1 text-center">
+                                                <div className="comon-top-l">
+                                                        <h5> Total Colletion </h5>
+                                                </div>
+                                                <div className="comon-top-b">
+                                                     <p> {colletion}</p>
+                                                </div>
+                                                
+                                                
+                                            </div>
+                                        </div>
+
+                                        <div className="col">
+                                            <div className="comon-lounched1 text-center">
+                                                <div className="comon-top-l">
+                                                        <h5> Contract Balance </h5>
+                                                </div>
+                                                <div className="comon-top-b">
+                                                    <p> {balance}</p>
+                                                    <h6 className="withdraw"> Withdraw </h6>
+                                                </div>
+                                                
+                                                
+                                            </div>
+                                        </div>
+                                   
                                     </div>
                                 </div>
                             </div>
-                            <div className="d-lg-flex justify-content-between align-items-center">
-                                <NavLink to={link} className="btn pre-btn">
-                                    Manage Allowlist
-                                </NavLink>
-                                <NavLink to={link} className="btn next-btn"> Manage <span> <BsArrowRight/> </span></NavLink>
-                            </div>
+                           
                         </div>
                     </div>
                     </>
--- src/pages/manage-mints/components/LounchedApi.js
+++ src/pages/manage-mints/components/LounchedApi.js
@@ -2,29 +2,47 @@ const Locncedata =[
 
     {
       id:1,
-      titel:"Gen 4 Mint",
+      titel:"Gen 1 Mint",
       time:"31st of December, 2021",
       link:"/managemints",
       editlink:"/managemints",
       colletion:"16000 ETH",
       balance:"6000 ETH",
+      owner:"200",
       auhor:"208 Owners",
-      pertage:" 700(87%)",
+      url:"jirafammint",
+      minted:" 700(87%)",
     
       
     },
 
     {
         id:2,
-        titel:"Gen 4 Mint",
+        titel:"Gen 2 Mint",
         time:"31st of December, 2021",
         link:"/managemints",
         editlink:"/managemints",
         colletion:"16000 ETH",
         balance:"6000 ETH",
+        owner:"200",
         auhor:"208 Owners",
-        pertage:" 700(87%)",
+        url:"jirafammint",
+        minted:" 700(87%)",
     },
+
+    {
+      id:3,
+      titel:"Gen 3 Mint",
+      time:"31st of December, 2021",
+      link:"/managemints",
+      editlink:"/managemints",
+      colletion:"16000 ETH",
+      balance:"6000 ETH",
+      owner:"200",
+      auhor:"208 Owners",
+      url:"jirafammint",
+      minted:" 700(87%)",
+  },
 
 
 
